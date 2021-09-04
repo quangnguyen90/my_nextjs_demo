fca49ecedf5457ab00fe841b2a911f2a7c65e5b5
quangnguyen90 <nguyenphuquang90@gmail.com>
2021-09-04 16:01:52 +0700

    2- Basic routes in Nextjs

---
+++ pages/profile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const profile = (props) => {
+  return <div>Profile page</div>;
+};
+
+profile.propTypes = {};
+
+export default profile;
